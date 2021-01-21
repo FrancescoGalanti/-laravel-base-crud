f30afb242c5490507222323b1ec80f9463fc5a68
FrancescoGalanti <galanti_francesco>
2021-01-21 16:30:35 +0100

    working

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+@section('content')
+   <div class="container mb-5">
+       <h1>SCHOOL HOME PAGE HERE</h1>
+
+       <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod nobis non iure tenetur expedita! Nisi sapiente voluptatibus nesciunt labore repellat quibusdam maxime, fuga a iure exercitationem? Nihil modi quos nemo!</P>
+   </div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,4 @@
+{{-- footer --}}
+<footer>
+  <p class="text-center">School site powered by class 19</p>
+</footer>
